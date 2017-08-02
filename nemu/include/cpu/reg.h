77511fdd697ebc73e70a2d14b8167aa386df3c11
tracer-ics2015 <tracer@njuics.org>
2017-08-02 17:48:29 +0800

    >  compile NEMU 141220000 star Linux star-pc 4.9.34-1-MANJARO #1 SMP PREEMPT Sat Jun 24 08:38:10 UTC 2017 i686 GNU/Linux  17:48:29 up  2:16,  1 user,  load average: 0.37, 0.34, 0.22 a12f12ea21e562a1df6872ba25d818c514b57bb2

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,7 +15,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
-	struct {
+	union {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
@@ -23,7 +23,8 @@ typedef struct {
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	uint32_t eax;
+	uint32_t ecx, edx, ebx, esp, ebp, esi, edi;
 
 
 	swaddr_t eip;
