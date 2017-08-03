32280c4180c4c95acc23d15aa8001bd78a557450
tracer-ics2015 <tracer@njuics.org>
2017-08-03 22:04:44 +0800

    >  compile NEMU 141220000 star Linux star-pc 4.9.34-1-MANJARO #1 SMP PREEMPT Sat Jun 24 08:38:10 UTC 2017 i686 GNU/Linux  22:04:44 up  6:24,  1 user,  load average: 0.96, 0.54, 0.41 1d0f097774868ee2202d102ebf15d8242517b73b

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -77,7 +77,7 @@ static bool make_token(char *e) {
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
-				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+				//Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
 
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
@@ -94,7 +94,7 @@ static bool make_token(char *e) {
 							assert(0);
 						int j;
 						for(j = 0; j < substr_len; j++)
-							tokens[nr_token].str[j] = e[position+j];
+							tokens[nr_token].str[j] = substr_start[j];
 						nr_token++;
 						break;
 					case OPENBAR:
