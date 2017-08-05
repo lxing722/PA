862832c64aaa7cdc62009d2c133ae43cf4db7e03
tracer-ics2015 <tracer@njuics.org>
2017-08-05 13:28:16 +0800

    >  compile NEMU 141220000 star Linux star-pc 4.9.34-1-MANJARO #1 SMP PREEMPT Sat Jun 24 08:38:10 UTC 2017 i686 GNU/Linux  13:28:16 up  2:56,  1 user,  load average: 0.70, 0.40, 0.24 e9c93b095f5c1872f517dd1328d97ae3bf56c47c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -155,23 +155,23 @@ static bool make_token(char *e) {
 }
 /////////////////////////////////////////////////////////////////////////////my code
 static int stack[MAX_SIZE];
-static unsigned int stack_len = 0;
+static unsigned int *stack_len = 0;
 static void init_stack(){
-	stack_len = 0;
+	*stack_len = 0;
 }
 static void push(int c){
-	if(stack_len == MAX_SIZE){
+	if(*stack_len == MAX_SIZE){
 		printf("Stack is full\n");
 		return;
 	}
-	stack[stack_len++] = c;
+	stack[(*stack_len)++] = c;
 }
 static int pop(){
-	if(stack_len == 0){
+	if(*stack_len == 0){
 		printf("Stack is empty\n");
 		return -1;
 	}	
-	return stack[stack_len--];
+	return stack[(*stack_len)--];
 }
 static bool check_parentheses(int start, int end){
 	init_stack();
@@ -182,7 +182,7 @@ static bool check_parentheses(int start, int end){
 		if(tokens[i].type == OPENBAR)
 			push(tokens[i].type);
 		if(tokens[i].type == CLOSEBAR){
-			if(stack_len == 1&&i != end)
+			if(*stack_len == 1&&i != end)
 				return false;
 			pop();
 		}
@@ -196,13 +196,13 @@ static bool check_bar(){
 		if(tokens[i].type == OPENBAR)
 			push(tokens[i].type);
 		else if(tokens[i].type == CLOSEBAR){
-			if(stack_len == 0)
+			if(*stack_len == 0)
 				return false;
 			pop();
 		}
 	}
-	printf("%d\n",stack_len);
-	if(stack_len != 0)
+	printf("%d\n",*stack_len);
+	if(*stack_len != 0)
 		return false;
 	return true;
 }
